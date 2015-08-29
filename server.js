2d8acfd49e326cfb9a9b1337c6df784a1392622d
Nguyen Hoang Son <nguyenhoangson@example.com>
2015-08-29 12:07:14 +0800

    Serve static files properly

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 (function(){
 
 	var express = require('express');
+	var path = require('path');
 	var app = express();
 
 	app	
+		.use("/app",express.static(path.join(__dirname + "/client/app")))
+		.use("/assets",express.static(__dirname + "/client/assets"))
 		.get('/', function(req, res){
-			res.send("Hello the world");
+			res.sendFile(__dirname + "/client/index.html");
 		})
 		.listen(8080);
 })();

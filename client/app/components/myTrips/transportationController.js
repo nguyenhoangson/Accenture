883730300238b407622e0f3eff23b1d7d9248260
Nguyen Hoang Son <nguyenhoangson@example.com>
2015-08-30 07:58:33 +0800

    Finish half transportation

--- client/app/components/myTrips/transportationController.js
+++ client/app/components/myTrips/transportationController.js
@@ -3,6 +3,14 @@
 
 	transportation
 		.controller("transportationController", ["$scope", function($scope){
-			
+			$scope.showFlight = function(){
+				$scope.transport = true;
+				$scope.flight = true;
+			};
+
+			$scope.showTransport = function(){
+				$scope.transport = false;
+				$scope.flight = false;
+			};
 		}]);
 })();
